7459ad7aca5aec4e0989a4c0414ae5134f490c04
frimarion <fr34m1337@gmail.com>
2026-03-07 16:19:28 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 window.SUPABASE_URL = "https://pzestsmgmtqcodwgepbu.supabase.co";
 window.SUPABASE_ANON_KEY = "sb_publishable_R4sLZqh8nJkY3-nUSU0zeg_0RzifKnu";
 window.ADMIN_PIN_HASH = "ed946f65d2c785d90e827c5ffd879ce3b49c68d4c88013074176a7e73bc58bcf"; // = "2580"
+window.GSHEETS_REPORT_URL = "https://script.google.com/macros/s/AKfycbxatuJZRG2MGRor6LMaW4We3p4jcBAwtt_DkOj-0gZbaSx03GsuAT4zSQJCnEiMZJvsbg/exec";
